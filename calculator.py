58ef283c76234f61f837ab899a62efbf6b479101
Young Suk Lee <42295097+BlueHarmel@users.noreply.github.com>
2020-02-04 16:56:32 +0900

    CodeIt Python study
    Starting the advanced level of python study

---
+++ calculator.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+def sum(x, y):
+    return x + y
+
+def difference(x, y):
+    return x - y
+
+def product(x, y):
+    return x * y
+
+def square(x):
+    return x * x
+
